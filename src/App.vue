f5c24b12a32195d9442aafdda108126c3233782e
Brian Retterer <bretterer@gmail.com>
2024-05-24 22:57:11 -0400

    Init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <header class="mx-auto mt-8" >
+    <img src="/Logo.jpeg" class="w-64 h-auto mx-auto" />
+  </header>
+  <div class="flex mt-12">
+    <button class="font-bangers mx-auto text-4xl bg-yellow-400/50 px-6 py-4 rounded-3xl">Start Game</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
